f414ac0004eb944d10d9c8e1922117d4eeac1757
Mohammadreza Zolfaghari <mohamadreza.zolfaghari@gmail.com>
2019-07-16 13:00:24 +0200

    Update run.sh

--- models_ECO_Full/hmdb51/run.sh
+++ models_ECO_Full/hmdb51/run.sh
@@ -3,7 +3,7 @@ mkdir -p snapshots
 mkdir -p training
 
 
-HDF5_DISABLE_VERSION_CHECK=1 /home/zolfagha/local/temporal_segmentNet/caffe-3D//build/tools/caffe train --solver=solver.prototxt --weights=/models/ECO_full_kinetics.caffemodel 2>&1 | tee -a training/log.txt
+HDF5_DISABLE_VERSION_CHECK=1 ../caffe_3d/build/tools/caffe train --solver=solver.prototxt --weights=/models/ECO_full_kinetics.caffemodel 2>&1 | tee -a training/log.txt
 
 
 
